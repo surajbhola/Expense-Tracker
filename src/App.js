6049fbc75d8aac09b2be6ac239dd87bfa10bb71e
Suraj Bhola <surajbhola1001@gmail.com>
2024-03-05 13:18:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 const DUMMY_EXPENSES = [
-  { id: "e1", title: "New TV", amount: 799.49, date: new Date(2024, 2, 12) },
-  {
-    id: "e2",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2024, 2, 28),
-  },
-  {
-    id: "e3",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2023, 5, 12),
-  },
+  // { id: "e1", title: "New TV", amount: 799.49, date: new Date(2024, 2, 12) },
+  // {
+  //   id: "e2",
+  //   title: "Car Insurance",
+  //   amount: 294.67,
+  //   date: new Date(2024, 2, 28),
+  // },
+  // {
+  //   id: "e3",
+  //   title: "New Desk (Wooden)",
+  //   amount: 450,
+  //   date: new Date(2023, 5, 12),
+  // },
 ];
 function App() {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
